4550c3020696230aa6a4584146edff8f8950bcc4
bdot100 <bidemiokunade@gmail.com>
2023-07-01 21:39:20 +0100

    User Register and login

--- shop/__init__.py
+++ shop/__init__.py
@@ -5,6 +5,7 @@ from flask_uploads import IMAGES, UploadSet, configure_uploads
 import os
 from werkzeug.utils import secure_filename
 from flask_msearch import Search
+from flask_login import LoginManager
 
 
 #basedir for our uploads
@@ -28,6 +29,13 @@ bcrypt = Bcrypt(app)
 search = Search()
 search.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view='customerLogin'
+login_manager.needs_refresh_message_category = 'danger'
+login_manager.login_message = u'Please login first'
+
 from shop.admin import routes
 from shop.products import routes
 from shop.carts import carts
+from shop.customers import routes
